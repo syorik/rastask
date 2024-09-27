81d93711ae13cd66b8267b8c5dce85a24bbe6402
Jaroslaw Sitarczuk <jaroslaw.k.sitarczuk@gmail.com>
2024-09-27 18:22:31 +0200

    add tailwind css

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,7 +1,16 @@
 import sveltePreprocess from 'svelte-preprocess'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
+  preprocess: sveltePreprocess({
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  }),
 }
